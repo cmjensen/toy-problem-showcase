61247d2433c5cfdd5ab3e4331c82f4e7507cd00e
Christie Jensen <christiejensen@Christies-MacBook-Air.local>
2021-01-20 14:06:48 -0700

    on step 3

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+class EvenAndOdd extends Component {
+    constructor(){
+        super();
+        this.state = {
+            evenArray: [],
+            oddArray: [],
+            userInput: ''
+        }
+    }
+
+    render(){
+        return (
+        <div className='puzzleBox evenAndOddPB'>
+            <h4>Evens and Odds</h4>
+            <input className='inputLine' onChange=''/>
+            <button className='confirmationButton'></button>
+            <span className='resultsBox'></span>
+            <span className='resultsBox'></span>
+        </div>
+        )
+    }
+}
+
+export default EvenAndOdd;
---
+++ src/components/Topics/Sum.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Sum extends Component {
+
+    render(){
+        return (
+        <p>Sum Component</p>
+        )
+    }
+}
+
+export default Sum;
