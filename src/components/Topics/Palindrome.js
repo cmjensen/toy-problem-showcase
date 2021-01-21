07dc4d7897a162df383eebd0994e648cae94252e
Christie Jensen <christiejensen@Christies-MacBook-Air.local>
2021-01-21 09:33:37 -0700

    Palindrome comment

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -20,7 +20,7 @@ class Palindrome extends Component {
     render(){
         return (
         <div className="puzzleBox filterStringPB">
-            <h4><Palindrome/h4>
+            <h4>Palindrome</h4>
             <input className="inputLine" onChange={(e) => this.handleChange(e.target.value)}></input>
             <button className="confirmationButton" onClick={() => this.checkPalindrome.userInput}></button>
             <span className="resultsBox"></span>
@@ -31,10 +31,10 @@ class Palindrome extends Component {
 
 export default Palindrome;
 
-The problem summary: Using a given string, determine if it is spelt the same backwards as it is forwards.
+// The problem summary: Using a given string, determine if it is spelt the same backwards as it is forwards.
 
-Create an onChange prop for the input element that updates the value of userInput on state.
-Create an onClick prop for the button element that calls a method on the class:
-This method should solve the toy problem.
-This method should update the value of palindrome.
-Assign the span element the value of palindrome.
+// Create an onChange prop for the input element that updates the value of userInput on state.
+// Create an onClick prop for the button element that calls a method on the class:
+// This method should solve the toy problem.
+// This method should update the value of palindrome.
+// Assign the span element the value of palindrome.
